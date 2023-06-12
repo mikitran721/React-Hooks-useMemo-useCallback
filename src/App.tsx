21ee2e643c235a060dca8a2aea4f0eb9d907e099
MikiTran <mikitran721@gmail.com>
2023-06-12 19:02:53 +0700

    su dung memo() cho child component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -14,11 +14,34 @@ function App() {
       <Todos todos={todos} />
       <hr />
       <div>
-        Count:{count}
-        <button onClick={increment}>INCREMENT</button>
+        Count:{count} <br />
+        <button
+          className="btn border bg-blue-500 text-white p-4 font-bold"
+          onClick={increment}
+        >
+          INCREMENT
+        </button>
       </div>
     </>
   );
 }
+
+const Todos = memo(({ todos }: { todos: string[] }) => {
+  console.log(`TODO RRENDERED`);
+  /**
+   * khi tang increment thi TODOS cung chay lai theo render cua APP;
+   * de tranh viec totos render theo app ta su dung memo(..) boc lai todos. tac dung voi component nhung vao - child comp
+   * export default memo(Todos); neu de file rieng
+   */
+
+  return (
+    <>
+      <h1 className="text-2xl font-semibold text-gray-700">MY TODOS</h1>
+      {todos.map((todo, index) => (
+        <p key={index}>{todo}</p>
+      ))}
+    </>
+  );
+});
 
 export default App;
